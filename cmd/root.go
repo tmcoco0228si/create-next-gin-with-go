8df2d26a287d3c97def368e2608744a331e4155f
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-02-14 07:30:23 +0900

    不要なコード修正

--- cmd/root.go
+++ cmd/root.go
@@ -1,36 +1,30 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
+var option string
+
 // ルートコマンド
 var rootCmd = &cobra.Command{
-	Use:   "create-next-gin-with-go_cli",
+	Use:   "nextjs-gin",
 	Short: "A brief description of your application",
 	Long:  `next-ginと記載すれば「next.js」と「gin」のインストールが開始されます`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// err := exec.Command("go", "get", "-u", "github.com/gin-gonic/gin").Run()
-		// err := exec.Command("npx", "create-next-app", "sample-app", " --typescript").Run()
-		// if err != nil {
-		// 	fmt.Println(err)
-		// }
-		fmt.Println("rootとしてきてるよ")
 	},
 }
 
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
-		fmt.Println(err)
 		os.Exit(1)
 	}
 }
 
 func init() {
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.create-next-gin-with-go_cli.yaml)")
-	rootCmd.Flags().StringP("next", "-n", "", "next.js作成時のコマンド")
+	//StringPと異なり、入力されたフラグを第一引数で直接使用することができる。
+	rootCmd.PersistentFlags().StringVar(&option, "option", "", "option")
 }
